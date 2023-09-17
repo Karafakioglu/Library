b32724e3927ba42e22fb191a532646051328ce7c
Karafakioglu <ismailkarafakioglu@gmail.com>
2023-09-17 23:39:00 +0200

    add new book functionality

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const myLibrary = [];
 const libraryElement = document.querySelector(".library");
+const addNewBookModalBtn = document.getElementById("addNewBookModalBtn");
+const addNewBookDialog = document.getElementById("addNewBookDialog");
+const addNewBookBtn = document.getElementById("addNewBookBtn");
+const inputs = document.querySelectorAll(".input");
+
 
 //Book constructor
-function Book(title, author, pagesRead, isRead){
+function Book(id, title, author, pagesRead, isRead){
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pagesRead = pagesRead;
@@ -10,15 +16,14 @@ function Book(title, author, pagesRead, isRead){
 }
 
 //Creating temp data
-let book1 = new Book("First Book", "Some Author", "112", "Not read");
-let book2 = new Book("Second Book", "Another Author", "0", "Not read");
+let book1 = new Book(1 ,"First Book", "Some Author", "112", "Not read");
+let book2 = new Book(2, "Second Book", "Another Author", "0", "Not read");
 
 myLibrary.push(book1,book2);
 
-//Creating dynamic list of the books
-myLibrary.forEach(book => {
+function renderBook(book) {
     const bookElem = `
-    <div class="book">
+    <div class="book" data-id="${book.id}">
         <ol>
             <li>${book.title}</li>
             <li>${book.author}</li>
@@ -26,10 +31,35 @@ myLibrary.forEach(book => {
             <li>${book.isRead}</li>
         </ol>
     </div>`;
-    console.log(bookElem)
-    libraryElement.insertAdjacentHTML('beforeend', bookElem)
+    libraryElement.insertAdjacentHTML('beforeend', bookElem);
+}
+
+
+//Creating dynamic list of the books
+myLibrary.forEach(book => {
+    renderBook(book);
 });
 
-function addBook(){
 
+function addBook(book){
+    myLibrary.push(book);
+    renderBook(book);  // Render the new book after adding
 }
+
+
+//Opens the modal window
+addNewBookModalBtn.addEventListener("click", () =>{
+    addNewBookDialog.showModal();
+})
+
+//handles the addnewbook or close button in modal
+addNewBookBtn.addEventListener("click", (e) => {
+    let tempObject = {};
+    e.preventDefault();
+    addNewBookDialog.close();
+
+    inputs.forEach(input => {
+        tempObject[input.id] = input.value
+    });
+    addBook(tempObject);
+})
